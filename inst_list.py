44e09936579c4a15cc8702835b68a3a872ca3e8b
Satch09 <18578121+Satch09@users.noreply.github.com>
2019-08-10 20:30:01 +0200

    fiddle and foo

--- inst_list.py
+++ inst_list.py
@@ -3,27 +3,38 @@ import re
 from openpyxl.chart import BarChart, Reference
 
 
-class oi_class:
+class dbm_instrument:
     def __init__(self, tag_name, tag_description, tag_io_type, tag_location):
         self.tag_name = tag_name
         self.tag_description = tag_description
         self.tag_io_type = tag_io_type
         self.tag_location = tag_location
+        self.all = {
+            'tag_name': self.tag_name,
+            'tag_description': self.tag_description,
+            'tag_io_type': self.tag_io_type,
+            'tag_location': self.tag_location,
+        }
 
 
-def process_workbook(input_filename):
+def create_io(input_filename):
     allIO = []
     wb = xl.load_workbook(input_filename, data_only=True)
     sheet = wb['Instrument List']
     for row in range(19, sheet.max_row + 1):
-        #print(sheet.cell(row, 5).value)
+        # Try to get the column range in the row to check which io type it is and record tag_io_type as such
+        """ row_range = str("P{}:V{}".format(row, row))
+        print(sheet.cell(row_range)) """
+
         if str(sheet.cell(row, 5).value).startswith("C1"):
             tag_name = sheet.cell(row, 5).value
             tag_description = str.strip(sheet.cell(row, 6).value)
+            tag_location = str.strip(sheet.cell(row, 7).value)
             # remove any whitespace
             cleaned_tag_name = re.sub('[\s+]', '', tag_name)
             # print(cleaned_cell)
-            ob = oi_class(cleaned_tag_name, tag_description, 'DI', 'here')
+            ob = dbm_instrument(
+                cleaned_tag_name, tag_description, 'DI', tag_location)
             ob_obj = {
                 "tag_name": cleaned_tag_name,
                 "tag_description": tag_description,
@@ -32,7 +43,12 @@ def process_workbook(input_filename):
             }
             # print(ob.tag_description)
             allIO.append(ob)
-    print(allIO[1].tag_name)
+    print(allIO[1].all)
+    sort_all_io = sorted(
+        allIO, key=lambda x: x.tag_name, reverse=False)
+    for i in sort_all_io:
+        print(i.tag_description)
+    print(allIO[1].all)
 
 
-process_workbook('inst_list.xlsx')
+create_io('inst_list.xlsx')
